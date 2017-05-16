70a55014e5c66877359b3c804afc73f4aa75cf03
YuTanaka <YRK00337@nifty.com>
2017-05-16 20:09:07 +0900

    part2の途中
    - タイトルですぐにゲーム開始するように
    - プレイヤーをオブジェクト化
    - 物理、アニメ、入力の有効設定

--- js/play.js
+++ js/play.js
@@ -1,9 +1,16 @@
 var playState = {
+    player: null,
   create: function() {
-      var player = game.add.sprite(10, 48, "characters");
-      player.frame = 0;
-      game.add.existing(player);
-      player.anchor.setTo(.5, 1);
+      var self = this;
+      self.player = game.add.sprite(300, 200, "characters");
+      self.player.frame = 4;
+      game.add.existing(self.player);
+      self.player.anchor.setTo(.5, 1);
+
+      self.player.animations.add('wait', [4], 1, true);       // 秒間1回切り替え。1パターンしかないのでアニメしないけど
+      game.physics.enable(self.player, Phaser.Physics.ARCADE);
+      game.input.activePointer.capture = true;
+
   },
   update: function() {
   }
